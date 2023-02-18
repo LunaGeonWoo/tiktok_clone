5e14adf2389b7c25571e474485bd9e6afbec3c66
GeonWoo <moongunwoo7019@naver.com>
2023-02-19 00:56:14 +0900

    added fontawesome

--- lib/features/authentication/widgets/auth_button.dart
+++ lib/features/authentication/widgets/auth_button.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class AuthButton extends StatelessWidget {
   final String text;
+  final FaIcon icon;
 
   const AuthButton({
     super.key,
     required this.text,
+    required this.icon,
   });
 
   @override
@@ -16,6 +19,7 @@ class AuthButton extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.symmetric(
           vertical: Sizes.size14,
+          horizontal: Sizes.size14,
         ),
         decoration: BoxDecoration(
           border: Border.all(
@@ -23,13 +27,22 @@ class AuthButton extends StatelessWidget {
             width: Sizes.size1,
           ),
         ),
-        child: Text(
-          text,
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            fontSize: Sizes.size16,
-            fontWeight: FontWeight.w600,
-          ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: icon,
+            ),
+            Text(
+              text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: Sizes.size16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
         ),
       ),
     );
